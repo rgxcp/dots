3cb43cb9591fb956554c5a9dfdbb9950947a1a8e
Rommy Gustiawan <rgcppp@gmail.com>
2020-12-23 16:47:35 +0700

    Mengubah script

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 #/bin/sh
-cp /etc/bluetooth/main.conf $HOME/Projects/dots/etc/bluetooth/main.conf
-cp /etc/modprobe.d/nobeep.conf $HOME/Projects/dots/etc/modprobe.d/nobeep.conf
-cp $HOME/.config/neofetch/config.conf $HOME/Projects/dots/.config/neofetch/config.conf
-cp $HOME/.i3/config $HOME/Projects/dots/.i3/config
-cp $HOME/.dmenurc $HOME/Projects/dots/.dmenurc
-cp $HOME/.Xresources $HOME/Projects/dots/.Xresources
-cp $HOME/.zshrc $HOME/Projects/dots/.zshrc
-echo "# All" > $HOME/Projects/dots/packages.txt
-pacman -Qe >> $HOME/Projects/dots/packages.txt
-echo -e "\n# AUR" >> $HOME/Projects/dots/packages.txt
-pacman -Qm >> $HOME/Projects/dots/packages.txt
+CWD=$HOME/Projects/dots
+
+cp /etc/bluetooth/main.conf $CWD/etc/bluetooth/main.conf
+cp /etc/modprobe.d/nobeep.conf $CWD/etc/modprobe.d/nobeep.conf
+cp $HOME/.config/neofetch/config.conf $CWD/.config/neofetch/config.conf
+cp $HOME/.i3/config $CWD/.i3/config
+cp $HOME/.dmenurc $CWD/.dmenurc
+cp $HOME/.Xresources $CWD/.Xresources
+cp $HOME/.zshrc $CWD/.zshrc
+
+echo "# All" > $CWD/packages.txt
+pacman -Qe >> $CWD/packages.txt
+echo -e "\n# AUR" >> $CWD/packages.txt
+pacman -Qm >> $CWD/packages.txt
