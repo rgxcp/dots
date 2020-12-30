0179a26dd1824bf101745ee149a7c98b39fb548d
Rommy Gustiawan <rgcppp@gmail.com>
2020-12-30 13:33:21 +0700

    Sinkronisasi ke-11

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ RICING
 | Distribution         | [Manjaro Linux i3](https://manjaro.org/download/#i3) |
 | Display Server       | [X.Org](https://www.x.org/wiki/)                     |
 | Window Manager       | [i3](https://i3wm.org/)                              |
+| Status Bar           | [i3status](https://i3wm.org/i3status/manpage.html)   |
 | Shell                | [zsh](https://www.zsh.org/)                          |
 | Application Launcher | [dmenu](https://tools.suckless.org/dmenu/)           |
 | Notification System  | [dunst](https://dunst-project.org/)                  |
--- dots.sh
+++ dots.sh
@@ -4,14 +4,15 @@ FLAG=$1
 update () {
     CWD=$HOME/Projects/dots
 
-    cp /etc/bluetooth/main.conf $CWD/etc/bluetooth/main.conf
-    cp /etc/modprobe.d/nobeep.conf $CWD/etc/modprobe.d/nobeep.conf
-    cp $HOME/.config/dunst/dunstrc $CWD/.config/dunst/dunstrc
-    cp $HOME/.config/i3/config $CWD/.config/i3/config
-    cp $HOME/.config/neofetch/config.conf $CWD/.config/neofetch/config.conf
-    cp $HOME/.dmenurc $CWD/.dmenurc
-    cp $HOME/.Xresources $CWD/.Xresources
-    cp $HOME/.zshrc $CWD/.zshrc
+    cat /etc/bluetooth/main.conf > $CWD/etc/bluetooth/main.conf
+    cat /etc/modprobe.d/nobeep.conf > $CWD/etc/modprobe.d/nobeep.conf
+    cat $HOME/.config/dunst/dunstrc > $CWD/.config/dunst/dunstrc
+    cat $HOME/.config/i3/config > $CWD/.config/i3/config
+    cat $HOME/.config/i3status/config > $CWD/.config/i3status/config
+    cat $HOME/.config/neofetch/config.conf > $CWD/.config/neofetch/config.conf
+    cat $HOME/.dmenurc > $CWD/.dmenurc
+    cat $HOME/.Xresources > $CWD/.Xresources
+    cat $HOME/.zshrc > $CWD/.zshrc
 
     echo "# All" > $CWD/packages.txt
     pacman -Qe >> $CWD/packages.txt
@@ -21,17 +22,18 @@ update () {
 
 replace () {
     CWD=$HOME/Projects/dots
-    read -p "Are you sure to replace the original config? [Y/n]: " CHOICE
+    read -p "Are you sure to replace the original config? [Y/n] " CHOICE
 
     if [ $CHOICE == "y" ] || [ $CHOICE == "Y" ]; then
-        cp $CWD/etc/bluetooth/main.conf /etc/bluetooth/main.conf
-        cp $CWD/etc/modprobe.d/nobeep.conf /etc/modprobe.d/nobeep.conf
-        cp $CWD/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
-        cp $CWD/.config/i3/config $HOME/.config/i3/config
-        cp $CWD/.config/neofetch/config.conf $HOME/.config/neofetch/config.conf
-        cp $CWD/.dmenurc $HOME/.dmenurc
-        cp $CWD/.Xresources $HOME/.Xresources
-        cp $CWD/.zshrc $HOME/.zshrc
+        cat $CWD/etc/bluetooth/main.conf > /etc/bluetooth/main.conf
+        cat $CWD/etc/modprobe.d/nobeep.conf > /etc/modprobe.d/nobeep.conf
+        cat $CWD/.config/dunst/dunstrc > $HOME/.config/dunst/dunstrc
+        cat $CWD/.config/i3/config > $HOME/.config/i3/config
+        cat $CWD/.config/i3status/config > $HOME/.config/i3status/config
+        cat $CWD/.config/neofetch/config.conf > $HOME/.config/neofetch/config.conf
+        cat $CWD/.dmenurc > $HOME/.dmenurc
+        cat $CWD/.Xresources > $HOME/.Xresources
+        cat $CWD/.zshrc > $HOME/.zshrc
     else
         echo "No operation were performed."
     fi
@@ -43,13 +45,18 @@ config () {
     case $FLAG in
         "dmenu") code $HOME/.dmenurc;;
         "dunst") code $HOME/.config/dunst/dunstrc;;
-        "i3") code $HOME/.i3/config;;
+        "i3") code $HOME/.config/i3/config;;
+        "i3status") code $HOME/.config/i3status/config;;
         "neofetch") code $HOME/.config/neofetch/config.conf;;
         "xres") code $HOME/.Xresources;;
         "zsh") code $HOME/.zshrc;;
         "") echo "Usage: dots <--config | -c> [file]";;
         *) echo "Unknown command";;
     esac
+}
+
+add_config () {
+    echo "Working..."
 }
 
 clear_cache () {
@@ -67,7 +74,8 @@ case $FLAG in
     "--update" | "-u") update;;
     "--replace" | "-r") replace;;
     "--config" | "-c") config $2;;
+    "--add-config" | "-ac") add_config $2 $3;;
     "--clear-cache" | "-cc") clear_cache $2;;
-    "") echo "Usage: dots [--update, -u] [--replace, -r] [--config, -c] [--clear-cache, -cc]";;
+    "") echo "Usage: dots [--update, -u] [--replace, -r] [--config, -c] [--add-config, -ac] [--clear-cache, -cc]";;
     *) echo "Unknown command";;
 esac
